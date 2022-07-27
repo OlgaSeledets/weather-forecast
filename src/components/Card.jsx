2f56d848520df871eb0f99641c4fd470f8c86ae9
Olga Seledets <olga.seledets@outlook.com>
2022-07-27 16:13:05 +0300

    feat: add temperature and icon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Card = ({weather}) => {
     return (
         <div className='card'>
-           {JSON.stringify(weather)}
+           <div className='temperature'>
+                {weather?.main?.temp ? Math.ceil(weather.main.temp) : ''}<span className='Celsius'>°C</span>
+           </div>
+           <img className='iconWeather' src={weather?.weather[0]?.icon ? `http://openweathermap.org/img/w/${weather.weather[0].icon}.png` : ''}/>
         </div>
     );
 }
