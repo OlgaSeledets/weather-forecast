58c0f1b7a27d1f3621cb40223c45b26354b1e1f0
Olga Seledets <olga.seledets@outlook.com>
2022-08-01 10:23:52 +0300

    feat: update function handleClick

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
-const Card = ({weather, visibility}) => {
+const Card = ({weather}) => {
     return (
-        <div className='card' style={{display: visibility}}>
+        <div className='card'>
            <div className='temperature'>
-                {weather?.main?.temp ? Math.ceil(weather.main.temp) : ''}<span className='Celsius'>°C</span>
+                {weather?.main?.temp ? Math.round(weather.main.temp) : ''}<span className='Celsius'>°C</span>
            </div>
            <img className='iconWeather' src={weather?.weather[0]?.icon ? `http://openweathermap.org/img/w/${weather.weather[0].icon}.png` : ''}/>
            <div className='description'>
                 {weather?.weather[0]?.description ? weather.weather[0].description : ''}
+                <br/>
+                {weather?.dt ? weather.dt : ''}
+                <br/>
+                {weather?.dt ? Number(weather.dt) + 24 * 3600 : ''}
            </div>
         </div>
     );
--- src/components/SearchWeather.jsx
+++ src/components/SearchWeather.jsx
@@ -8,29 +8,22 @@ const API_KEY = "39c13462afcea0dacb3084d61bb29812";
 const SearchWeather = () => {
     const [city, setCity] = useState();
     const [weather, setWeather] = useState();
-    const [visibility, setVisibility] = useState('none');
-    const [clickTodayBtn, setClickTodayBtn] = useState(true);
+    const [isVisibility, setIsVisibility] = useState(false);
 
+    const handleClick = () => {
+        setIsVisibility(true)
+    }
     const getWeather = (city) => {
-        const handleClick = () => {
-            if (clickTodayBtn === true) {
-                setVisibility('')
-                setClickTodayBtn(false)
-            }
-            else {
-                setVisibility('none')
-                setClickTodayBtn(true)
-            }
-        }
         return async () => { 
             const response = await fetch(
-            `${URL}?q=${city}&appid=${API_KEY}&exclude=minutely,hourly,daily,alerts&units=metric`
+            `${URL}?q=${city}&appid=${API_KEY}&units=metric`
             );
             const data = await response.json();
             
             setWeather(data);
             console.log(data);
             handleClick()
+            console.log(`https://api.openweathermap.org/data/2.5/forecast?appid=39c13462afcea0dacb3084d61bb29812&q=London&units=metric&cnt=16`)
         }
     };
 
@@ -38,10 +31,12 @@ const SearchWeather = () => {
         <div>
             <h1 className='title'>Weather Forecast</h1>
             <input onChange={e => setCity(e.target.value)} className='search' type="text" placeholder='Search...'></input>
-            <Button title='TODAY' style='btn' action={getWeather(city)} />
-            <Button title='TOMORROW' style='btn' />
+            <Button name='today' title='TODAY' style='btn' action={getWeather(city)} />
+            <Button name='tomorrow' title='TOMORROW' style='btn' action={getWeather('London')} />
             <Button title='WEEK' style='btn btn-week' />
-            <Card weather={weather} visibility={visibility} />
+            {
+                (isVisibility === true) && <Card weather={weather} />
+            }
         </div>
     );
 }
